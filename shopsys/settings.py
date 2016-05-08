b7de2f6eeed94edffec2283e06af85782ed648e0
valexchao <254587453@qq.com>
2016-05-08 05:50:55 +0000

    migrate to mysql

--- shopsys/settings.py
+++ shopsys/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'shopsys.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shopsys',
+        'USER': 'shopsys',
+        'PASSWORD':'888444',
     }
 }
 
