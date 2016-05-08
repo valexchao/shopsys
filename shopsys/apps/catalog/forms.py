97fbba6db40fb95e557467d88a84c9c880d55c6f
valexchao <254587453@qq.com>
2016-05-08 15:38:00 +0000

    增加admin和验证

---
+++ shopsys/apps/catalog/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Product
+
+
+class ProductAdminForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        exclude = []
+
+    # 验证属性price
+    def clean_price(self):
+        if self.cleaned_data['price'] <= 0:
+            raise forms.ValidationError('价格必须大于0.')
+        return self.cleaned_data['price']
